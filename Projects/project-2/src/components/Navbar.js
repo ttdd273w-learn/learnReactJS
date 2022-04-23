9f63fb281c5747148f848be42be79b1abc725c5d
Tong Duan <td273@cornell.edu>
2022-04-23 14:57:33 -0400

    Started project 2

---
+++ Projects/project-2/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AirbnbLogo from "../images/airbnb_logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={AirbnbLogo} alt="Airbnb Logo" />
+    </nav>
+  );
+}
