6b21aa648db662204086f0114d8a6c1d7e27bd8c
Tong Duan <td273@cornell.edu>
2022-06-09 22:12:11 -0500

    Continued with useEffect:

--- Projects/api-calls/src/App.js
+++ Projects/api-calls/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(1);
 
-  console.log("Component rendered");
+  // console.log("Component rendered");
 
   // fetch("https://swapi.dev/api/people/1")
   //   .then((res) => res.json())
@@ -12,18 +12,18 @@ export default function App() {
 
   // side effects
   React.useEffect(() => {
-    fetch("https://swapi.dev/api/people/1")
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarWarsData(data));
-  }, []);
+  }, [count]);
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       <h2>The count is {count}.</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Add
+        Get Next Character
       </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
