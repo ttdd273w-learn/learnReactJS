d797f003c8712d9290e3b4a296f34d6c098d8b2e
Tong Duan <td273@cornell.edu>
2023-01-07 18:44:25 -0500

    Continuing with state

---
+++ moz-todo-react/src/components/Form.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function Form(props) {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name) {
+      props.addTask(name);
+      setName("");
+    } else {
+      alert("Task must not be empty!");
+    }
+  }
+
+  return (
+    <form>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button
+        onClick={handleSubmit}
+        type="submit"
+        className="btn btn__primary btn__lg"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
